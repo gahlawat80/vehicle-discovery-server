063cbbf350e0929efd4c9e2662711c298fd7c63c
gahla <gahlawat80@gmail.com>
2022-12-21 18:41:42 -0500

    discovery server

---
+++ src/test/java/com/learn2code/discovery/server/registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn2code.discovery.server.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
